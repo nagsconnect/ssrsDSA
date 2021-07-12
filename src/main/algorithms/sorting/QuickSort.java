df7193966fd376062daa2b8f11c185dbe2b7736f
SantoshPenjArla <p25santosh@gmail.com>
2021-07-12 15:30:44 +0530

    Added Sorting algo - QuickSort with tests

---
+++ src/main/algorithms/sorting/QuickSort.java
@@ -0,0 +1,49 @@
+package main.algorithms.sorting;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+public class QuickSort {
+
+    List<Integer> list;
+
+    public QuickSort(List<Integer> list) {
+        this.list = list;
+    }
+
+    public void sort() {
+        divideAndSort(0, list.size() - 1);
+    }
+
+    private void divideAndSort(int left, int right) {
+        if (left < right) {
+            int partitionIndex = partition(left, right);
+
+            divideAndSort(left, partitionIndex - 1);
+            divideAndSort(partitionIndex + 1, right);
+        }
+    }
+
+    private int partition(int left, int right) {
+        int pivot = this.list.get(right);
+
+        int index = left - 1;
+        int startIndex = left;
+        int endIndex = right - 1;
+        while (startIndex <= endIndex) {
+            int currentElement = this.list.get(startIndex);
+            if (currentElement < pivot) {
+                index++;
+                this.list.set(startIndex, this.list.get(index));
+                this.list.set(index, currentElement);
+            }
+            startIndex++;
+        }
+        this.list.set(right, this.list.get(index + 1));
+        this.list.set(index + 1, pivot);
+        return index + 1;
+    }
+
+}
---
+++ src/test/algorithms/sorting/QuickSortTest.java
@@ -0,0 +1,39 @@
+package algorithms.sorting;
+
+import main.algorithms.sorting.QuickSort;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class QuickSortTest {
+
+    @Test
+    public void test_QuickSort_WhenListIsUnsortedRandomly() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(4, 1, 7, 15, 2, 7, 9));
+        List<Integer> sortedList = new ArrayList<>(Arrays.asList(1, 2, 4, 7, 7, 9, 15));
+        QuickSort quickSort = new QuickSort(list);
+        quickSort.sort();
+        Assert.assertEquals(list, sortedList);
+    }
+
+    @Test
+    public void test_QuickSort_WhenListIsSorted_IncreasingOrder() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 4, 7, 7, 9, 15));
+        List<Integer> sortedList = new ArrayList<>(Arrays.asList(1, 2, 4, 7, 7, 9, 15));
+        QuickSort quickSort = new QuickSort(list);
+        quickSort.sort();
+        Assert.assertEquals(list, sortedList);
+    }
+
+    @Test
+    public void test_QuickSort_WhenListIsSorted_DecreasingOrder() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(15, 9, 7, 7, 4, 2, 1));
+        List<Integer> sortedList = new ArrayList<>(Arrays.asList(1, 2, 4, 7, 7, 9, 15));
+        QuickSort quickSort = new QuickSort(list);
+        quickSort.sort();
+        Assert.assertEquals(list, sortedList);
+    }
+}
